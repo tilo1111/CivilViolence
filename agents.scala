28d5aedde541876da83251b004cfb88b197a984a
filipjaszczyk <65018905+tilo1111@users.noreply.github.com>
2021-01-15 15:46:27 +0100

    Add files via upload

--- agents.scala
+++ agents.scala
@@ -1,53 +1,93 @@
+import scala.util.Random
+import scala.math._
+
+
 abstract class Agent
 {
   val name: String
-  val vision: Float
-  def move(): Unit
-  def action(): Unit
+  val vision: Int
+  var state: Int
+  val grievance: Double
+  def move(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int): Unit
+  def action(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int, T:Double): Unit
 }
 
-class Cop (v: Float) extends Agent
+class Cop (v: Int) extends Agent
 {
-  override val vision: Float = v
-  override val name: String = "COP"
+  override val vision: Int = v
+  override val name: String = " COP "
+  var state: Int = 2
+  val grievance: Double = 0
 
-  def move(): Unit =
+  def move(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int): Unit =
   {
-
+    //move to random position in range which is empty
+    val emptys = range.filter(f => matrix(f._1)(f._2).name == "EMPTY")
+    if (emptys.size != 0)
+    {
+      val newPos = emptys(Random.nextInt(emptys.size))
+      val tmp = matrix(newPos._1)(newPos._2)
+      matrix(newPos._1)(newPos._2) = matrix(i)(j)
+      matrix(i)(j) = tmp
+    } 
   }
 
-  def action(): Unit =
+  def action(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int, T:Double): Unit =
   {
-
+    //do research if there are any activists arrest one randomly
+    val activist = range.filter(f => matrix(f._1)(f._2).name == "CIVIL").filter(f => matrix(f._1)(f._2).state == 1)
+    if(activist.size > 0)
+    {
+      val prisoner = activist(Random.nextInt(activist.size))
+      matrix(prisoner._1)(prisoner._2).state = -1 // jail is -1
+    }
   }
 }
 
-class Civil (H: Double, L: Double, R: Double, v: Float) extends Agent
+class Civil (H: Double, L: Double, R: Double, v: Int) extends Agent
 {
   val hardship: Double = H
   val legitimacy: Double = L
   val riskAversion: Double = R
-  override val vision: Float = v
+  override val vision: Int = v
   override val name: String = "CIVIL"
 
   val grievance: Double = hardship*(1-legitimacy)
-  val state: Int = 0    // 0 - Quiet; 1 - Active
+  var state: Int = 0    // 0 - Quiet; 1 - Active; -1 - Jail
 
-  def move(): Unit =
+  def move(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int): Unit =
   {
-
+    //move to random position in range which is empty
+    val emptys = range.filter(f => matrix(f._1)(f._2).name == "EMPTY")
+    if (emptys.size != 0)
+    {
+      val newPos = emptys(Random.nextInt(emptys.size))
+      val tmp = matrix(newPos._1)(newPos._2)
+      matrix(newPos._1)(newPos._2) = matrix(i)(j)
+      matrix(i)(j) = tmp
+    }  
   }
 
-  def action(): Unit =
-  {
-
+  def action(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int, T: Double): Unit =
+  {  
+    //if grievance - risk*arrest probability > T active else quiet
+    //arrest probability = 1 - exponent(-k* [cops/active agents (including me)] <- in vision range)
+    val k = log(10)
+    val C = range.filter(f => matrix(f._1)(f._2).name == "COP").size
+    val A = 1+range.filter(f => matrix(f._1)(f._2).name == "CIVIL").filter(f => matrix(f._1)(f._2).state == 1).size
+    val P = 1.0 - exp(-k*(1.0*C/A))
+    val condition = grievance - riskAversion*P
+    matrix(i)(j).state = if(condition>T) 1 else 0  
   }
+  
 }
 
 class EmpA () extends Agent
 {
-  val vision: Float = 0
+  var state: Int = 0
+  val vision: Int = 0
+  val grievance: Double = 0
   override val name: String = "EMPTY"
-  def move(): Unit = {}
-  def action(): Unit = {}
+  def move(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int): Unit = {}
+  def action(matrix: Array[Array[Agent]], range: List[(Int, Int)], i: Int, j: Int, T:Double): Unit = {}
 }
--- main.scala
+++ main.scala
@@ -7,12 +7,13 @@ object main {
   val r = scala.util.Random         //r.nextFloat
 
   val L: Int = 10
-  var legitimacy: Double = 0.9
+  var legitimacy: Double = 0.001
   var threshold: Double = 0.5
   val alfa: Int = 0               // 0 or 1
+  val T = 0.5
   var jailTerm: Int = 1^alfa
   var civilDens: Double = 0.5
-  var copDens:Double = 0.05
+  var copDens:Double = 0.02
   var visionCops = 1
   var visionCivils = 1
 
@@ -27,13 +28,38 @@ object main {
 
   def printMatrix(matrix: Array[Array[Agent]],  L: Int): Unit =
     {
-      for (i <- 0 until L)
+      for (i <- 0 until L) {
       {
         for (j <- 0 until L)
           print(matrix(i)(j).name + "  ")
         println()
       }
+      println()
+      }
     }
+
+  def printGrievance(matrix: Array[Array[Agent]],  L: Int): Unit =
+  {
+    for (i <- 0 until L) {
+      for (j <- 0 until L) {
+        print((matrix(i)(j).grievance*100).toInt/100.0 + "  ")
+      }
+      println()
+    }
+    println()
+  }
+
+  def printState(matrix: Array[Array[Agent]],  L: Int): Unit =
+  {
+    for (i <- 0 until L) {
+      for (j <- 0 until L) {
+        print(matrix(i)(j).state + "  ")
+      }
+      println()
+    }
+    println()
+  }
+
 
   def fillMatrix(matrix: Array[Array[Agent]],  L: Int, civilDens: Double, copDens: Double): Unit =
     {
@@ -46,7 +72,7 @@ object main {
           val y = r.nextInt(L)
           if (matrix(x)(y).name == "EMPTY")
             {
-              matrix(x)(y) = new Civil(r.nextFloat(), L, r.nextFloat(), visionCivils)
+              matrix(x)(y) = new Civil(r.nextFloat(), legitimacy, r.nextFloat(), visionCivils)
               numOfCivils -= 1
             }
         }
@@ -62,6 +88,64 @@ object main {
         }
       }
     }
+  
+  def copsPos(matrix: Array[Array[Agent]], L: Int): List[(Int, Int)] =
+  {
+    var copsList: List[(Int, Int)] = List()
+    for (i <- 0 until L)
+    {
+        for (j <- 0 until L)
+        {
+            if (matrix(i)(j).name == "COP")
+            {
+              copsList :+= (i,j)
+            }
+        }
+    }
+    return copsList
+  }
+  
+  def move(matrix: Array[Array[Agent]]): Unit =
+  {
+    for (i <- 0 until L)
+    {
+        for (j <- 0 until L)
+        {
+            if (matrix(i)(j).name == " COP " || matrix(i)(j).name == "CIVIL")
+            {
+              matrix(i)(j).move(matrix, range(i,j,L,matrix(i)(j).vision), i, j)
+            }          
+        }
+    }
+  }
+  
+  def action(matrix: Array[Array[Agent]]): Unit =
+  {
+    for (i <- 0 until L)
+    {
+        for (j <- 0 until L)
+        {
+            if (matrix(i)(j).name == " COP " || matrix(i)(j).name == "CIVIL")
+            {
+              matrix(i)(j).action(matrix, range(i,j,L,matrix(i)(j).vision), i, j, T)
+            }          
+        }
+    }
+  }
+  
+  def range(i: Int, j: Int, L: Int, vision: Int): List[(Int, Int)] =
+  {
+    val range = (0 until L).toList
+    val up = if(i - vision>=0) range.slice(i-vision,i) else range.take(i) ++ range.takeRight((i - vision).abs)
+    val down = if(i + vision<L) range.slice(i+1,i+1+vision) else range.take((i + vision) - L+1) ++ range.slice(i+1,L)
+    val left = if(j - vision>=0) range.slice(j-vision,j) else range.take(j) ++ range.takeRight((j - vision).abs)
+    val right = if(j + vision<L) range.slice(j+1,j+1+vision) else range.take((j + vision) - L+1) ++ range.slice(j+1,L)
+    val vertical = (up++down).map(f => (f,j))
+    val horizontal = (left++right).map(f => (i,f))
+    val neighbours = vertical ++ horizontal
+    return neighbours 
+  }
+  
 
   def main(args:Array[String]): Unit =
   {
@@ -69,7 +153,17 @@ object main {
     var matrix = Array.ofDim[Agent](L,L)
     emptyMatrix(matrix, L)
     fillMatrix(matrix, L, civilDens, copDens)
-    printMatrix(matrix, L)
+    printGrievance(matrix, L)
+    printState(matrix, L)
+
+    for (i <- 0 until 10)
+    {
+      action(matrix)
+      move(matrix)
+      printState(matrix, L)
+    }
+
+    printGrievance(matrix, L)
 
 
   }
